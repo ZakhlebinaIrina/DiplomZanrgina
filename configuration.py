46a9fa8d606dae8c4e812d3b020198e30e61d72f
LAPTOP-3KLNSSQH\Ирина <i.emelyanova585@yandex.ru>
2023-10-16 22:47:41 +0300

    Diplomniy proekt

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Ирина Занегина, 9-я когорта — Финальный проект. Инженер по тестированию плюс
+URL_SERVER = 'https://0a5e40db-90db-4583-8d86-7faab68b7425.serverhub.praktikum-services.ru'
+CREATE_ORDERS = '/api/v1/orders'
+GET_ORDERS = '/v1/orders/track'
